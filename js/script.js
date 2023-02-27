590ae192cce27db78a914a7c22c909656f3640b6
LeandraMT <magantierleandra@gmail.com>
2023-02-27 19:50:57 +0100

    Rearranged some of the code piece

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// Creating the content within the the container
+let li = $('<ul></ul>');
+let inputValue = $('#input').val();
+li.append(inputValue);
+
+if (inputValue === ''){
+    alert('You must write something');
+} else {
+    $('#list').append(li);
+}
+
+// Adding the delete button on the item list
+let crossOutButton = $('<crossOutButton></crossOutButton>');
+crossOutButton.append(document.createTextNode('x'));
+li.append(crossOutButton);
+
+// Event listener for the X button
+crossOutButton.on('click', deleteListItem);
+    function deleteListItem(){
+        li.addClass('delete')
+    }
+
+// Creating the function for deleting the items
+function deleteListItem() {
+   li.toggleClass('delete');
+}
+
+// Crossing out an item from the list
+function crossOut(){
+    li.toggleClass('strike');
+}
+
+// Adding the event listener to the crossOut item
+li.on('dblclick', function crossOut() {
+    li.toggleClass('strike');
+});
+
+// Reorder the items on the list
+$('#list').sortable();
