f01fa4217af39946182d3c53b3ecab7d29245499
LeandraMT <magantierleandra@gmail.com>
2023-02-28 01:07:54 +0100

    debugging main function "newItem()"

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-// Creating the content within the the container
-let li = $('<li></li>');
-let inputValue = $('#input').val();
-li.append(inputValue);
+function newItem(){
 
-if (inputValue === ''){
-    alert('You must write something');
-} else {
-    $('#list').append(li);
+    // Creating the content within the the container
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === ''){
+        alert('You must write something');
+    } else {
+        $('#list').append(li);
+    }
+
+    // Adding the delete button on the item list
+    let crossOutButton = $('<crossOutButton>x</crossOutButton>');
+    li.append(crossOutButton);
+
+    // Event listener for the X button
+    crossOutButton.on('click', deleteListItem);
+
+    // Creating the function for deleting the items
+    function deleteListItem() {
+    li.addClass('delete');
+    }
+
+    // Crossing out an item from the list
+    function crossOut(){
+        li.toggleClass('strike');
+    }
+
+    // Adding the event listener to the crossOut item
+    li.on('dblclick',  function crossOut(){
+        li.toggleClass('strike');
+    });
+
+    // Reorder the items on the list
+    $('#list').sortable();
 }
-
-// Adding the delete button on the item list
-let crossOutButton = $('<crossOutButton></crossOutButton>');
-crossOutButton.append(document.createTextNode('x'));
-li.append(crossOutButton);
-
-// Event listener for the X button
-crossOutButton.on('click', deleteListItem);
-
-// Creating the function for deleting the items
-function deleteListItem() {
-   li.toggleClass('delete');
-}
-
-// Crossing out an item from the list
-function crossOut(){
-    li.toggleClass('strike');
-}
-
-// Adding the event listener to the crossOut item
-li.on('dblclick', function crossOut() {
-    li.toggleClass('strike');
-});
-
-// Reorder the items on the list
-$('#list').sortable();
